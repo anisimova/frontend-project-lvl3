1e1a52bd0d871669e4bc93f50ddfe8a7be6942fc
Nadezhda Anisimova <nadya.anisimova@gmail.com>
2022-03-07 19:09:46 +0300

    3th step - add i18n and error texts

--- src/app.js
+++ src/app.js
@@ -1,40 +1,59 @@
 import * as yup from 'yup';
 import onChange from 'on-change';
+import i18n from 'i18next';
+import resources from './locales/ru.js';
 
-const isValid = (enteredUrl, urls) => yup.string().url().notOneOf(urls).validate(enteredUrl);
+const app = (i18nextInstance) => {
+  yup.setLocale({
+    string: {
+      url: () => ({ key: 'invalidUrl' }),
+    },
+    mixed: {
+      notOneOf: () => ({ key: 'notOneOf' }),
+    },
+  });
+  const isValid = (enteredUrl, urls) => yup.string().url().notOneOf(urls).validate(enteredUrl);
 
-const handleProcessState = (elements, processState) => {
-  switch (processState) {
-    case 'error':
-      elements.rssUrl.classList.add('is-invalid');
-      break;
+  const handleProcessState = (elements, processState) => {
+    switch (processState) {
+      case 'error':
+        elements.rssUrl.classList.add('is-invalid');
+        break;
 
-    case 'waiting':
-      elements.rssUrl.classList.remove('is-invalid');
-      elements.rssUrl.focus();
-      elements.form.reset();
-      break;
+      case 'waiting':
+        elements.rssUrl.classList.remove('is-invalid');
+        elements.rssUrl.focus();
+        elements.form.reset();
+        break;
 
-    case 'filling':
-      // elements.submitButton.disabled = false;
-      break;
+      case 'filling':
+        break;
 
-    default:
-      throw new Error(`Unknown process state: ${processState}`);
-  }
-};
+      case 'success':
+        break;
 
-const render = (elements) => (path, value, prevValue) => {
-  switch (path) {
-    case 'form.processState':
-      handleProcessState(elements, value);
-      break;
+      default:
+        throw new Error(`Unknown process state: ${processState}`);
+    }
+  };
+  const renderErrors = (elements, err) => {
+    const feedbackElement = elements.feedback;
+    feedbackElement.textContent = i18nextInstance.t(`feedback.error.${err}`);
+  };
+  const render = (elements) => (path, value) => {
+    switch (path) {
+      case 'form.processState':
+        handleProcessState(elements, value);
+        break;
 
-    default:
-      break;
-  }
-};
-export default () => {
+      case 'form.errors':
+        renderErrors(elements, value);
+        break;
+
+      default:
+        break;
+    }
+  };
   const elements = {
     form: document.querySelector('.rss-form'),
     rssUrl: document.querySelector('#url-input'),
@@ -64,11 +83,23 @@ export default () => {
       .then(() => {
         state.form.addedUrls.push(state.form.enteredUrl);
         watchedState.form.processState = 'waiting';
-        console.log(state.form.addedUrls);
+        // console.log(state.form.addedUrls);
       })
       .catch((err) => {
-        console.log(err);
+        // console.log(err);
+        const [{ key }] = err.errors;
         watchedState.form.processState = 'error';
+        watchedState.form.errors = key;
       });
   });
 };
+const runApp = () => {
+  const i18nextInstance = i18n.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: true,
+    resources,
+  }).then(() => app(i18nextInstance));
+};
+
+export default runApp;
---
+++ src/locales/ru.js
@@ -0,0 +1,13 @@
+export default {
+  ru: {
+    translation: {
+      feedback: {
+        successfully: 'RSS успешно загружен',
+        error: {
+          invalidUrl: 'Ресурс не содержит валидный RSS',
+          notOneOf: 'RSS уже существует',
+        },
+      },
+    },
+  },
+};
